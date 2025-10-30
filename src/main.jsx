cd28d5e0ccccf9dcf632c44117f284213feebaef
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-30 10:50:45 +0600

    json data update

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Cards from './Components/Cards/Cards.jsx';
 import Error from './Components/Error/Error.jsx';
 
 
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -34,7 +35,7 @@ const router = createBrowserRouter([
         path: "contact",
         element: <Contacts></Contacts>
       },
-  
+      
      
     ]
   },
